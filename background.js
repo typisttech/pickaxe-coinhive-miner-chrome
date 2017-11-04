bee6a49a921a2c8e9f85e155b3f986ba1c4fe161
Tang Rufus <tangrufus@gmail.com>
2017-11-05 05:37:30 +0800

    Add options page

--- background.js
+++ background.js
@@ -1,19 +1,15 @@
+import {
+  DONATE_SITE_KEY,
+  DEFAULT_OPTIONS,
+} from './constants.js';
 import Miners from './miners.js';
 
-const DONATE_SITE_KEY = 'I2z6pueJaeVCz5dh1uA8cru5Fl108DtH';
-const DEFAULT_OPTIONS = Object.freeze({
-  isEnabled: true,
-  mainSiteKey: DONATE_SITE_KEY,
-  mainThrottle: 0.2,
-  referralSiteKey: DONATE_SITE_KEY,
-});
-
-const minerConfig = (siteKey, userNameSuffix, throttle) => ({
+const minerConfig = (siteKey, userNameSuffix, speed) => ({
   siteKey,
   userName: `Pickaxe Coinhive Miner Chrome: ${userNameSuffix}`,
   options: {
     autoThreads: 'auto',
-    throttle,
+    throttle: ((100 - speed) / 100),
   },
 });
 
@@ -21,21 +17,25 @@ const minerConfig = (siteKey, userNameSuffix, throttle) => ({
 const miners = new Miners([]);
 
 const pickaxe = () => {
-  chrome.storage.local.get(['isEnabled', 'mainSiteKey', 'mainThrottle', 'referralSiteKey'], (storage) => {
+  chrome.storage.local.get(['isEnabled', 'mainSiteKey', 'mainSpeed', 'referrerSiteKey'], (storage) => {
     console.group('Pickaxe');
     const options = Object.assign({}, DEFAULT_OPTIONS, storage);
 
     miners.reset([
-      minerConfig(options.mainSiteKey, 'Main', options.mainThrottle),
-      minerConfig(options.referralSiteKey, 'Referral', 0.05),
-      minerConfig(DONATE_SITE_KEY, 'Donate', 0.1),
+      minerConfig(options.mainSiteKey, 'Main', options.mainSpeed),
+      minerConfig(options.referrerSiteKey, 'Referrer', 5),
+      minerConfig(DONATE_SITE_KEY, 'Donate', 10),
     ]);
 
     if (options.isEnabled && navigator.onLine) {
-      chrome.browserAction.setIcon({ path: 'icons/icon48.png' });
+      chrome.browserAction.setIcon({
+        path: 'icons/icon48.png',
+      });
       miners.start();
     } else {
-      chrome.browserAction.setIcon({ path: 'icons/icon48-grayscale.png' });
+      chrome.browserAction.setIcon({
+        path: 'icons/icon48-grayscale.png',
+      });
     }
 
     console.groupEnd();
@@ -44,7 +44,9 @@ const pickaxe = () => {
 
 const toggleIsEnabled = () => {
   chrome.storage.local.get('isEnabled', (options) => {
-    chrome.storage.local.set({ isEnabled: !options.isEnabled });
+    chrome.storage.local.set({
+      isEnabled: !options.isEnabled,
+    });
   });
 };
 
